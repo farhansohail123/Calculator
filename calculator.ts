9f217b6fb4279ca63b5f97ed71b13d183dfd643c
farhansohail123 <muhammad.farhan19dec@gmail.com>
2023-11-07 23:55:27 +0500

    Initial commit

---
+++ calculator.ts
@@ -0,0 +1,47 @@
+import { sum } from "./Operations/add.js"
+import { sub } from "./Operations/sub.js"
+import { mul } from "./Operations/mul.js"
+import { div } from "./Operations/div.js"
+
+import inquirer from "inquirer"
+let answer: { num1: number, num2: number, operator: string } = await inquirer.prompt(
+    [{
+        message: "Enter your first Number : ",
+        type: "number",
+        name: "num1",
+    },
+    {
+        message: "Enter your second Number : ",
+        type: "number",
+        name: "num2",
+    },
+    {
+        message: "select operator : ",
+        type: "list",
+        choices: ["+", "-", "*", "/"],
+        name: "operator"
+    }]
+);
+
+let { num1, num2, operator } = answer;
+console.log(num1);
+
+if (num1 && num2 && operator) {
+    let result: number;
+    if (operator === "+") {
+        let result = sum(answer.num1, answer.num2);
+        console.log("Result : ", result);
+
+    } else if (operator === "-") {
+        let result = sub(answer.num1, answer.num2);
+        console.log("Result : ", result);
+
+    } else if (operator === "*") {
+        let result = mul(answer.num1, answer.num2);
+        console.log("Result : ", result);
+
+    } else if (operator === "/") {
+        let result = div(answer.num1, answer.num2);
+        console.log("Result : ", result);
+    }
+}
